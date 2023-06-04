555847ccf0da360b92b3214723fc3f59bfb319e4
Arsha5502 <arshamjoby@gmail.com>
2023-06-04 12:26:25 +0530

    commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, url_for, redirect, session
+import pymongo
+import bcrypt
+#set app as a Flask instance 
+app = Flask(__name__)
+#encryption relies on secret keys so they could be run
+app.secret_key = 
+#connoct to your Mongo DB database
+client = pymongo.MongoClient("mongodb+srv://admin:admin@cluster0.epqxvmj.mongodb.net/?retryWrites=true&w=majority")
+
+#get the database name
+db = client.get_database('total_records')
+#get the particular collection that contains the data
+records = db.register
